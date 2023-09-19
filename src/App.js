2ce237094abd43264b78cf86eb51b5ac1472eb6a
Sumit <104936009+Ksumit1109@users.noreply.github.com>
2023-09-19 19:56:05 +0530

    Using useReduce instead of useState .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import User from './components/User/User'
-import React from 'react';
+import React, { useState , useEffect} from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(()=>{
+    const userLogInfo = localStorage.getItem("isLoggedIn");
+
+    if(userLogInfo === "1"){
+      setIsLoggedIn(true)
+    }
+  },[])
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", 1)
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn")
+    setIsLoggedIn(false);
+  };
+
   return (
     <React.Fragment>
-      <User></User>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
     </React.Fragment>
   );
 }
